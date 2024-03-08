3610dc9aa3554deff907d58aa27cdad2c69149f4
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-08 16:56:09 +0900

    Create abc086_a.hx

---
+++ abc086_a.hx
@@ -0,0 +1,24 @@
+//abc086_a.hx
+################################
+################################
+################################
+################################
+################################
+################################
+import haxe.io.Input;
+
+class Main {
+    static function main() {
+        var input:Input = Sys.stdin();
+        var ab = [for( t in input.readLine().split(' ') ) Std.parseInt(t)];
+        var a = ab[0];
+        var b = ab[1];
+        if((a * b) % 2 == 1) {
+            Sys.println('Odd');
+        }
+        else {
+            Sys.println('Even');
+        }
+    }
+}
+################################
