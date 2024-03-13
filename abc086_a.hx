778450fbf4e44848af7deb67432b2095d4d80802
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-14 00:11:38 +0900

    Update abc086_a.hx

--- abc086_a.hx
+++ abc086_a.hx
@@ -4,9 +4,23 @@
 ################################
 ################################
 ################################
+import haxe.io.Input;
+class Main {
+    static function main() {
+        var input:Input = Sys.stdin();
+        var ab = [for( t in input.readLine().split(' ') ) Std.parseInt(t)];
+        var a = ab[0];
+        var b = ab[1];
+        if((a * b) % 2 == 1) {
+            Sys.println('Odd');
+        }
+        else {
+            Sys.println('Even');
+        }
+    }
+}
 ################################
 import haxe.io.Input;
-
 class Main {
     static function main() {
         var input:Input = Sys.stdin();
